cd28db35e8b21a13acc3b9abad538a8adc2189d6
sidhub <sidharthbng@gmail.com>
2019-02-15 08:26:26 +0530

    from home 2

---
+++ files.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+
+void main(){
+	FILE* file = fopen("sidh.txt", "a");
+	
+	int prev_s[50];
+	strcpy(prev_s, "000");
+	char s[50];
+	strcpy(s, "000");
+	
+	gets(s);
+	puts("-");
+	puts(s);
+	puts("-");
+	return;
+		
+	do {
+		gets(s);
+		
+	}while(1);
+	fclose(file);
+}
+
