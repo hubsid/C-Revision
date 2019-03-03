498e625f372b12178110fa78eb24a593b7eb8638
sidhub <sidharthbng@gmail.com>
2019-03-03 07:31:13 +0530

    f

---
+++ e.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void main(){
+printf("%s,%s,%s,%d", __DATE__,__TIME__, __FILE__, __LINE__);
+}
+
--- files.c
+++ files.c
@@ -1,24 +1,62 @@
 #include<stdio.h>
 #include<string.h>
 
-void main(){
-	FILE* file = fopen("sidh.txt", "a");
+#define BLOCK_N 20
+
+struct wordlist {
+	char **first;
+	int len;
+};
+
+char* fget_str(char* file_name){
+	FILE* file = fopen(file_name, "r");
+	if(!file) {
+		printf("file cannot be opened.");
+		return NULL;
+	}
 	
-	int prev_s[50];
-	strcpy(prev_s, "000");
-	char s[50];
-	strcpy(s, "000");
+	char str[BLOCK_N];
+	int n = 1;
+	int len;
+	char *cp = malloc(1);
 	
-	gets(s);
-	puts("-");
-	puts(s);
-	puts("-");
-	return;
-		
-	do {
-		gets(s);
-		
-	}while(1);
+	while(fgets(str, BLOCK_N, file)) {
+		len = strlen(str);
+		char *cp1 = (char*)realloc(cp, n+len);
+		if(!cp1) {
+			printf("Problems  reallocating memory...");
+			break;
+		}
+		cp = cp1;
+		strcpy(cp + n-1, str);
+		n += len;
+	}
 	fclose(file);
+	return cp;
+}
+
+struct wordlist* fget_str_ps(char* file_name, char* delim) {
+	FILE* file = fopen(file_name, "r");
+	if(!file) {
+		printf("file cannot be opened.");
+		return NULL;
+	}
+	
+	char str[BLOCK_N];
+	char ** cp = malloc(sizeof(char*));
+	int n = 0;
+	int str_len;
+	
+	while(fgets(str, BLOCK_N, file)) {
+		str_len = strlen(str);
+		for(int i = 0; i < str_len; i++) {
+			
+		}
+	}
+	
+	struct wordlist *list = malloc(sizeof(struct wordlist));
+	list->first = cp;
+	list->len = n;
+	return list;
 }
 
