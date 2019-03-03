cd7733c8ae1498523ea910debd1011d8fb6c7978
sidhub <sidharthbng@gmail.com>
2019-03-03 21:25:37 +0530

    asdf

--- files.c
+++ files.c
@@ -42,21 +42,56 @@ struct wordlist* fget_str_ps(char* file_name, char* delim) {
 		return NULL;
 	}
 	
-	char str[BLOCK_N];
+	char buf_str[BLOCK_N];
 	char ** cp = malloc(sizeof(char*));
 	int n = 0;
 	int str_len;
 	
-	while(fgets(str, BLOCK_N, file)) {
-		str_len = strlen(str);
-		for(int i = 0; i < str_len; i++) {
-			
+	while(fgets(buf_str, BLOCK_N, file)) {
+		puts("block");
+		char *p, *prev_p;
+		for(p = buf_str, prev_p = buf_str; *p != '\0'; p++) {
+			for(char *p1 = delim; *p1 != '\0'; p1++) {
+				if(*p1 == *p) {
+					puts("match");
+					int len = p - prev_p;
+					if(len > 0) {
+						char* temp_str = malloc(len + 1);
+						printf("*prev_p=%c,*p=%c,temp_str before copy=%s\n", *p, *prev_p, temp_str);
+						strncpy(temp_str, prev_p, len);
+						temp_str[len] = '\0';
+						printf("len=%d\ntemp_str=%s\n", len, temp_str);
+						char** cp_tmp = (char**)realloc(cp, ++n * sizeof(char*));
+						
+						if(!cp_tmp) {
+							printf("realloc failed...");
+							exit(0);
+						}
+						cp = cp_tmp;
+						cp[n-1] = temp_str;
+					}
+					prev_p = p+1;
+					break;
+				}
+			}	
 		}
+//		if(prev_p != p-1)
 	}
 	
 	struct wordlist *list = malloc(sizeof(struct wordlist));
 	list->first = cp;
 	list->len = n;
 	return list;
+}
+
+int main() {
+	
+	struct wordlist *list = fget_str_ps("inputfile.txt", " ");
+	printf("size:%d\n", list->len);
+	
+	for(int i = 0; i < list->len; i++)
+		printf("%d\t%s\n", strlen(list->first[i]), list->first[i]);
+	
+	return 0;
 }
 
