47f3c6dcd85a490805829015c2d6c3d995d59506
SIDHARTH R <sidharth.r@netcracker.com>
2019-03-04 12:24:41 +0530

    Revert "Update files.c"
    This reverts commit fe357d7640d5cf0fe1e2f51c6a5224552a18d59a.

--- files.c
+++ files.c
@@ -1,1 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+
+void main(){
+	FILE* file = fopen("sidh.txt", "r");
+	if(!file) {
+		puts("the file pointer is null");
+		return;
+	}
+	char s[100];
+	
+	char* strs = malloc(1);
+	*strs = '\0';
+	char* cp = strs;
+	int len;
+	int size = 0;
+	while(fgets(s, 100, file) != NULL) {
+		len = strlen(s);
+		printf("len=%d, size=%d\n", len, size);
+		size += len;
+		strs = realloc(strs, size+1);
+		if(!strs) {
+			puts("not able to allocate memory, when trying to copy the string :");
+			puts(s);
+			break;
+		}
+		strcpy(cp, s);
+		printf("+len=%d, size=%d\n", len, size);
+		cp += len;
+		
+		printf("allocation size:%d bytes\n", size);
+	}
+	puts("the file is :");
+	puts(strs);
+	fclose(file);
+}
 
