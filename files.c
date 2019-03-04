65feb557773ed15a73f45d7345ade457b9c69c98
hubsid <47071868+hubsid@users.noreply.github.com>
2019-03-04 12:42:43 +0530

    revert files.c to the change made from home
    snapshot when string splitting is working.

--- files.c
+++ files.c
@@ -1,24 +1,114 @@
 #include<stdio.h>
 #include<string.h>
 
-void main(){
-	FILE* file = fopen("sidh.txt", "a");
+#define BLOCK_N 20
+
+struct wordlist {
+	char **first;
+	int len;
+};
+
+char* fget_str(char* file_name){
+	FILE* file = fopen(file_name, "r");
+	if(!file) {
+		printf("file cannot be opened.");
+		return NULL;
+	}
 	
-	int prev_s[50];
-	strcpy(prev_s, "000");
-	char s[50];
-	strcpy(s, "000");
+	char str[BLOCK_N];
+	int n = 1;
+	int len;
+	char *cp = malloc(1);
 	
-	gets(s);
-	puts("-");
-	puts(s);
-	puts("-");
-	return;
-		
-	do {
-		gets(s);
-		
-	}while(1);
+	while(fgets(str, BLOCK_N, file)) {
+		len = strlen(str);
+		char *cp1 = (char*)realloc(cp, n+len);
+		if(!cp1) {
+			printf("Problems  reallocating memory...");
+			break;
+		}
+		cp = cp1;
+		strcpy(cp + n-1, str);
+		n += len;
+	}
 	fclose(file);
+	return cp;
 }
 
+struct wordlist* fget_str_ps(char* file_name, char* delim) {
+	FILE* file = fopen(file_name, "r");
+	if(!file) {
+		printf("file cannot be opened.");
+		return NULL;
+	}
+	
+	char buf_str[BLOCK_N];
+	char *overflow = NULL;
+	char ** cp = malloc(sizeof(char*));
+	int n = 0;
+	int str_len;
+	
+	while(fgets(buf_str, BLOCK_N, file)) {
+		puts("block");
+		char* p = buf_str, *prev_p = buf_str;
+		if(overflow) {
+			for(; *p != '\0'; p++) {
+				for(char *p1 = delim; *p1 != '\0'; p1++) {
+					if(*p1 == *p) {
+						int len = p - prev_p;
+						if(len > 0) {
+							char* temp_str = malloc(len + 1);
+						}
+						
+					}
+				}
+			}
+		}
+		for(; *p != '\0'; p++) {
+			for(char *p1 = delim; *p1 != '\0'; p1++) {
+				if(*p1 == *p) {
+					puts("match");
+					int len = p - prev_p;
+					if(len > 0) {
+						char* temp_str = malloc(len + 1);
+						printf("*prev_p=%c,*p=%c,temp_str before copy=%s\n", *p, *prev_p, temp_str);
+						strncpy(temp_str, prev_p, len);
+						temp_str[len] = '\0';
+						printf("len=%d\ntemp_str=%s\n", len, temp_str);
+						char** cp_tmp = (char**)realloc(cp, ++n * sizeof(char*));
+						
+						if(!cp_tmp) {
+							printf("realloc failed...");
+							exit(0);
+						}
+						cp = cp_tmp;
+						cp[n-1] = temp_str;
+					}
+					prev_p = p+1;
+					break;
+				}
+			}	
+		}
+		if(prev_p != p) {
+			int len = p - prev_p;
+			overflow = malloc(len);
+			strncpy(overflow, prev_p, len);
+		}
+	}
+	
+	struct wordlist *list = malloc(sizeof(struct wordlist));
+	list->first = cp;
+	list->len = n;
+	return list;
+}
+
+int main() {
+	
+	struct wordlist *list = fget_str_ps("inputfile.txt", " ");
+	printf("size:%d\n", list->len);
+	
+	for(int i = 0; i < list->len; i++)
+		printf("%d\t%s\n", strlen(list->first[i]), list->first[i]);
+	
+	return 0;
+}
