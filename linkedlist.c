be307a1ef512ee967a3f799784f3dfac1c7e7125
SIDHARTH R <sidharth.r@netcracker.com>
2019-03-19 13:25:49 +0530

    sdkjfh

--- linkedlist.c
+++ linkedlist.c
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-#include<stdio.h>
-
-typedef struct node{
-	int key;
-	struct node *next;
-} Node;
-
-Node *head = NULL;
-
-void add(int key) {
-	Node p = malloc(sizeof(Node));
-	if(!head) {
-		head = p;
-	}
-	else
-}
-
-int main(int argc, char const *argv[])
-{
-	return 0;
-}
-=======
 #include <stdio.h>
 #include <stdlib.h>
 
